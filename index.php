fb7c8524cd9219cdd27571842a02b75dda4b7d05
Matt <matty_noye@live.com>
2018-07-12 02:09:51 +0100

    Added in hero
    I have added in the hero to the top of the page.

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php include'includes/header.php'; ?>
 
+<section id="hero" class="hero">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="hero-inner">
+					<h3 class="text-uppercase">
+						Brand new Portfolio template
+					</h3>
+					<h1>
+						Freshport
+					</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					</p>
+					<a href="#about" class="btn btn-info btn-lg get-started link-scroll">About us</a>
+				</div><!-- hero-inner -- >
+			</div><!-- col -->
+		</div><!-- row -->
+	</div><!-- container -->
+</section><!-- closes hero section -->
 
 <?php include'includes/footer.php'; ?>
