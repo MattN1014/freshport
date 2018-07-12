1b82e8a7ec0dc4b5f9ce2c9c602a50bcf0e5573c
Matt <matty_noye@live.com>
2018-07-12 03:18:28 +0100

    Added in the about section & content
    I have created the about section & also added in dummy content to this section

--- index.php
+++ index.php
@@ -21,4 +21,25 @@
 	</div><!-- container -->
 </section><!-- closes hero section -->
 
+<section id="about" class="about">
+	<div class="container text-center">
+		<div class="row">
+			<div class="col-md-4">
+				<img class="img-fluid" src="assets/img/user.png">
+			</div>
+			<div class="col">
+				<h2>
+					About Us
+				</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in porta metus, consequat viverra massa. Mauris vel efficitur ex. Vivamus dapibus odio eget lobortis interdum. Nunc sed imperdiet ante. In bibendum risus eget condimentum posuere. Suspendisse mollis eget velit eget rutrum. Morbi aliquet accumsan metus, ac consectetur nulla luctus sed.
+				</p>
+				<p>
+					Aenean ut luctus felis. Nulla tincidunt, risus ut interdum finibus, odio turpis luctus nulla, nec interdum turpis sem eget sapien. Nunc libero velit, vulputate at lobortis vel, vulputate sit amet magna. Etiam lectus velit, pulvinar vel placerat id, finibus nec urna. Quisque pretium, nulla vitae gravida interdum, massa elit ornare odio, condimentum sollicitudin mi erat ut ligula.
+				</p>
+			</div>
+		</div><!-- row -->
+	</div><!-- container -->
+</section><!-- closes about section -->
+
 <?php include'includes/footer.php'; ?>
