5254fb8b04afd667c5bb94f35035735110b7ce5c
Matt <matty_noye@live.com>
2018-07-14 23:04:32 +0100

    Portfolio modals page
    This page holds all of the modals for the portfolio section

---
+++ includes/modals/portfolio.php
@@ -0,0 +1,271 @@
+<div id="my-port-item1" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
+    <div role="document" class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" data-dismiss="modal" aria-label="Close" class="close">
+            	<span aria-hidden="true">
+            		<i class="fa fa-times"></i>
+            	</span>
+            </button>
+            <div class="container-fluid">
+              	<div class="row align-items-center">
+	                <div class="port-mockup col-md-6">
+	                      <img src="assets/img/port/item-1.jpg" class="img-fluid">
+	                </div>
+	                <div class="text col-md-6 text-center text-md-left ">
+		                <h2>Demo portfolio item name</h2>
+		                <p class="lead">
+		                	You can create these popups easily. Every portfolio item will need one. Also, no need to worry about the Macbook mockups. Just paste a photo, template will do the rest.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <a href="#" class="btn btn-primary btn-lg">
+		                	Visit website
+		                </a>
+	                </div><!-- col -->
+              	</div><!-- row -->
+            </div><!-- container-fluid -->
+        </div><!-- modal-content -->
+    </div><!-- modal-dialog -->
+</div><!-- my-port-item1 -->
+
+<div id="my-port-item2" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
+    <div role="document" class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" data-dismiss="modal" aria-label="Close" class="close">
+            	<span aria-hidden="true">
+            		<i class="fa fa-times"></i>
+            	</span>
+            </button>
+            <div class="container-fluid">
+              	<div class="row align-items-center">
+	                <div class="port-mockup col-md-6">
+	                      <img src="assets/img/port/item-2.jpg" class="img-fluid">
+	                </div>
+	                <div class="text col-md-6 text-center text-md-left ">
+		                <h2>Demo portfolio item name</h2>
+		                <p class="lead">
+		                	You can create these popups easily. Every portfolio item will need one. Also, no need to worry about the Macbook mockups. Just paste a photo, template will do the rest.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <a href="#" class="btn btn-primary btn-lg">
+		                	Visit website
+		                </a>
+	                </div><!-- col -->
+              	</div><!-- row -->
+            </div><!-- container-fluid -->
+        </div><!-- modal-content -->
+    </div><!-- modal-dialog -->
+</div><!-- my-port-item1 -->
+
+<div id="my-port-item3" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
+    <div role="document" class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" data-dismiss="modal" aria-label="Close" class="close">
+            	<span aria-hidden="true">
+            		<i class="fa fa-times"></i>
+            	</span>
+            </button>
+            <div class="container-fluid">
+              	<div class="row align-items-center">
+	                <div class="port-mockup col-md-6">
+	                      <img src="assets/img/port/item-3.jpg" class="img-fluid">
+	                </div>
+	                <div class="text col-md-6 text-center text-md-left ">
+		                <h2>Demo portfolio item name</h2>
+		                <p class="lead">
+		                	You can create these popups easily. Every portfolio item will need one. Also, no need to worry about the Macbook mockups. Just paste a photo, template will do the rest.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <a href="#" class="btn btn-primary btn-lg">
+		                	Visit website
+		                </a>
+	                </div><!-- col -->
+              	</div><!-- row -->
+            </div><!-- container-fluid -->
+        </div><!-- modal-content -->
+    </div><!-- modal-dialog -->
+</div><!-- my-port-item1 -->
+
+<div id="my-port-item4" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
+    <div role="document" class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" data-dismiss="modal" aria-label="Close" class="close">
+            	<span aria-hidden="true">
+            		<i class="fa fa-times"></i>
+            	</span>
+            </button>
+            <div class="container-fluid">
+              	<div class="row align-items-center">
+	                <div class="port-mockup col-md-6">
+	                      <img src="assets/img/port/item-4.jpg" class="img-fluid">
+	                </div>
+	                <div class="text col-md-6 text-center text-md-left ">
+		                <h2>Demo portfolio item name</h2>
+		                <p class="lead">
+		                	You can create these popups easily. Every portfolio item will need one. Also, no need to worry about the Macbook mockups. Just paste a photo, template will do the rest.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <a href="#" class="btn btn-primary btn-lg">
+		                	Visit website
+		                </a>
+	                </div><!-- col -->
+              	</div><!-- row -->
+            </div><!-- container-fluid -->
+        </div><!-- modal-content -->
+    </div><!-- modal-dialog -->
+</div><!-- my-port-item1 -->
+
+<div id="my-port-item5" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
+    <div role="document" class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" data-dismiss="modal" aria-label="Close" class="close">
+            	<span aria-hidden="true">
+            		<i class="fa fa-times"></i>
+            	</span>
+            </button>
+            <div class="container-fluid">
+              	<div class="row align-items-center">
+	                <div class="port-mockup col-md-6">
+	                      <img src="assets/img/port/item-5.jpg" class="img-fluid">
+	                </div>
+	                <div class="text col-md-6 text-center text-md-left ">
+		                <h2>Demo portfolio item name</h2>
+		                <p class="lead">
+		                	You can create these popups easily. Every portfolio item will need one. Also, no need to worry about the Macbook mockups. Just paste a photo, template will do the rest.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <a href="#" class="btn btn-primary btn-lg">
+		                	Visit website
+		                </a>
+	                </div><!-- col -->
+              	</div><!-- row -->
+            </div><!-- container-fluid -->
+        </div><!-- modal-content -->
+    </div><!-- modal-dialog -->
+</div><!-- my-port-item1 -->
+
+<div id="my-port-item6" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
+    <div role="document" class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" data-dismiss="modal" aria-label="Close" class="close">
+            	<span aria-hidden="true">
+            		<i class="fa fa-times"></i>
+            	</span>
+            </button>
+            <div class="container-fluid">
+              	<div class="row align-items-center">
+	                <div class="port-mockup col-md-6">
+	                      <img src="assets/img/port/item-6.jpg" class="img-fluid">
+	                </div>
+	                <div class="text col-md-6 text-center text-md-left ">
+		                <h2>Demo portfolio item name</h2>
+		                <p class="lead">
+		                	You can create these popups easily. Every portfolio item will need one. Also, no need to worry about the Macbook mockups. Just paste a photo, template will do the rest.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <a href="#" class="btn btn-primary btn-lg">
+		                	Visit website
+		                </a>
+	                </div><!-- col -->
+              	</div><!-- row -->
+            </div><!-- container-fluid -->
+        </div><!-- modal-content -->
+    </div><!-- modal-dialog -->
+</div><!-- my-port-item1 -->
+
+<div id="my-port-item7" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
+    <div role="document" class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" data-dismiss="modal" aria-label="Close" class="close">
+            	<span aria-hidden="true">
+            		<i class="fa fa-times"></i>
+            	</span>
+            </button>
+            <div class="container-fluid">
+              	<div class="row align-items-center">
+	                <div class="port-mockup col-md-6">
+	                      <img src="assets/img/port/item-7.jpg" class="img-fluid">
+	                </div>
+	                <div class="text col-md-6 text-center text-md-left ">
+		                <h2>Demo portfolio item name</h2>
+		                <p class="lead">
+		                	You can create these popups easily. Every portfolio item will need one. Also, no need to worry about the Macbook mockups. Just paste a photo, template will do the rest.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <a href="#" class="btn btn-primary btn-lg">
+		                	Visit website
+		                </a>
+	                </div><!-- col -->
+              	</div><!-- row -->
+            </div><!-- container-fluid -->
+        </div><!-- modal-content -->
+    </div><!-- modal-dialog -->
+</div><!-- my-port-item1 -->
+
+<div id="my-port-item8" tabindex="-1" role="dialog" aria-hidden="true" class="modal fade">
+    <div role="document" class="modal-dialog">
+        <div class="modal-content">
+            <button type="button" data-dismiss="modal" aria-label="Close" class="close">
+            	<span aria-hidden="true">
+            		<i class="fa fa-times"></i>
+            	</span>
+            </button>
+            <div class="container-fluid">
+              	<div class="row align-items-center">
+	                <div class="port-mockup col-md-6">
+	                      <img src="assets/img/port/item-8.jpg" class="img-fluid">
+	                </div>
+	                <div class="text col-md-6 text-center text-md-left ">
+		                <h2>Demo portfolio item name</h2>
+		                <p class="lead">
+		                	You can create these popups easily. Every portfolio item will need one. Also, no need to worry about the Macbook mockups. Just paste a photo, template will do the rest.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <p>
+		                	Maxime animi quia distinctio magnam, deleniti officiis quibusdam cupiditate sed id labore at, odio odit recusandae assumenda ea impedit autem nisi minima.
+		                </p>
+		                <a href="#" class="btn btn-primary btn-lg">
+		                	Visit website
+		                </a>
+	                </div><!-- col -->
+              	</div><!-- row -->
+            </div><!-- container-fluid -->
+        </div><!-- modal-content -->
+    </div><!-- modal-dialog -->
+</div><!-- my-port-item1 -->
