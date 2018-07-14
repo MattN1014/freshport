0f8dbecf2f2178dd42682ce2e4b84d2db57f3c72
Matt <matty_noye@live.com>
2018-07-14 01:09:41 +0100

    Script added to stop URL updating on menu link clik
    I have added a piece of code which will stop the URL from being updated when a menu item is clicked.  
    
    This will stop the page are from being displayed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,10 +13,17 @@ $(function (){
         target: '.navbar',
         offset: 120
     });
+
+    //Stop URL updating on nav link click
+    $('.link-scroll').bind('click', function (e) {
+
+        e.preventDefault();
+
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top - 80
+        }, 1000);
+    });
+
 });
-
-
-
-
-
 
