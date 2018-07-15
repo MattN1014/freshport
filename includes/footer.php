64095e7f5fbc6668d757554b3a78a3481afd34da
Matt <matty_noye@live.com>
2018-07-15 22:37:51 +0100

    Footer top added
    I have added a footer top class to the footer.  This holds a newsletter signup form and also contact info

--- includes/footer.php
+++ includes/footer.php
@@ -1,22 +1,43 @@
 	</div><!-- closes site-wrapper -->
-    <section id="contact">
-
-    </section>
 	<footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 copyright">
+        <div class="footer-top">
+            <div class="container text-center">
+                <div class="newsletter">
+                    <form id="subscription-form" action="" method="post">
+                        <div class="form-group">
+                            <input type="text" name="sub-email" placeholder="Subscribe to our newsletter">
+                            <input type="submit" value="Subscribe Now">
+                        </div><!-- form-group -->
+                    </form>
+                </div><!-- newsletter -->
+                <div class="contact-info">
                     <p>
-                        &copy; <?php echo date('Y'); ?> Your company | All Rights Reserved
+                        123 Lewisham High Street, London, United Kingdom
                     </p>
-                </div>
-                <div class="col-md-6 des-by">
                     <p>
-                        Designed by <a target="_blank" href="https://www.mwn-design.co.uk">MWN Design</a>
+                        <a href="mailto:team@yourcompany.com">
+                            team@yourcompany.com
+                        </a>
                     </p>
-                </div>
-            </div>
-        </div>
+                </div><!-- contact-info -->
+            </div><!-- container -->
+        </div><!-- footer-top -->
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 copyright">
+                        <p>
+                            &copy; <?php echo date('Y'); ?> Your company | All Rights Reserved
+                        </p>
+                    </div>
+                    <div class="col-md-6 des-by">
+                        <p>
+                            Designed by <a target="_blank" href="https://www.mwn-design.co.uk">MWN Design</a>
+                        </p>
+                    </div>
+                </div><!-- row -->
+            </div><!-- container -->
+        </div><!-- footer-bottom -->
 	</footer>
 	<!-- link in JS files -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
